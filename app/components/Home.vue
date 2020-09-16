2dd3459b7fdd5d65b6a8ae8fb5f66a8138be5c5a
Tam Mai <tam.mai@bigin.vn>
2020-09-16 12:54:14 +0700

    feat(all): initial commit

---
+++ app/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <Page>
+    <ActionBar title="Home" />
+    <StackLayout class="container" android:clipToPadding="false">
+      <StackLayout class="card" androidElevation="10">
+        <Label class="h3" :text="message" />
+      </StackLayout>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 16;
+  background-color: #f0f0f0;
+}
+
+.card {
+  border-radius: 12;
+  background-color: #ffffff;
+  padding: 24;
+  height: 100;
+  vertical-align: center;
+}
+
+.h3 {
+  color: #444444;
+}
+</style>
